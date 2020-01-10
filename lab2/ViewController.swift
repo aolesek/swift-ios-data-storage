59c8246bb10f383d72bb03b1e2c2f64f2f41e7c1
aolesek <arkadiusz.olesek@gmail.com>
2020-01-10 09:40:46 +0100

    Initial Commit

---
+++ lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by arek on 10/01/2020.
+//  Copyright © 2020 aolesek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
